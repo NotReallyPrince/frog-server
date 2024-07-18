3395c17614d359639aab4994f0eff33a00da18e6
Muhsin Neyyathur <muhsinny333@gmail.com>
2024-07-19 00:34:12 +0530

    fix: cors issue

--- main.ts
+++ main.ts
@@ -8,7 +8,7 @@ import cors from 'cors'
 const app: Express = express()
 const PORT = process.env.PORT || 5200
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://frog-client.netlify.app'],
   credentials: true,
 };
 
