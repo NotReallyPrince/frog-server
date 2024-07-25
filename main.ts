b0b479efc97a543cde39b1eafcf6b01cfc5e2e9c
rishadpt <rishadpt16@gmail.com>
2024-07-26 00:51:20 +0530

    feat(ratelimit): added

--- main.ts
+++ main.ts
@@ -4,6 +4,7 @@ import bot from './src/bot'
 import { userRoute } from './src/routes'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { rateLimit } from 'express-rate-limit'
 
 const app: Express = express()
 const PORT = process.env.PORT || 5200
@@ -11,7 +12,14 @@ const corsOptions = {
   origin: ['http://localhost:5173', 'https://frog-frontend.vercel.app'],
   credentials: true,
 };
-
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+})
+app.use(limiter)
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); // Preflight requests
 
