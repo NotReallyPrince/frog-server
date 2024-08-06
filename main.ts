29a3c606ecde4298384993619de7a99cc847ef24
rishadpt <rishadpt16@gmail.com>
2024-08-06 09:52:17 +0530

    fix

--- main.ts
+++ main.ts
@@ -41,18 +41,14 @@ app.options('*', cors(corsOptions)); // Preflight requests
 app.use(express.urlencoded({ limit: 50 * 1024 * 1024 }));
 app.use(express.json({ limit: '50mb' }));
 app.use(bodyParser.json());
-app.set('trust proxy', 1);
+// app.set('trust proxy', 1);
 
 app.use(
   cookieSession({
     name: "session",
     keys: ['adahdhjadad$$' as any],
     maxAge: 60 * 24 * 60 * 60 * 100,
-    secureProxy:true,
-    secure:true,
-    domain:'api.apescommunity.com',
-    httpOnly:true,
-    sameSite:'lax'
+  
   })
 );
 
