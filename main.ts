3fcc078f4f02310bf833d644ae5453479f3afe33
rishadpt <rishadpt16@gmail.com>
2024-08-06 08:44:34 +0530

    fix

--- main.ts
+++ main.ts
@@ -47,7 +47,6 @@ app.use(
   cookieSession({
     name: "session",
     keys: ['adahdhjadad$$' as any],
-    domain:'.apescommunity.com',
     sameSite:'none',
     maxAge: 60 * 24 * 60 * 60 * 100,
     path:'/',
